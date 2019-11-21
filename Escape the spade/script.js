ad3bb3f95e6bc525d118c9bc767706fe14bff4b0
Rishit Patel <49016509+imrishit98@users.noreply.github.com>
2019-11-21 13:22:58 -0500

    Add files via upload

---
+++ Escape the spade/script.js
@@ -0,0 +1,16 @@
+function startGame() {
+    ranNum1 = Math.floor((Math.random() * 4) + 1);
+    ranNum2 = Math.floor((Math.random() * 4) + 1);
+    ranNum3 = Math.floor((Math.random() * 4) + 1);
+    ranNum4 = Math.floor((Math.random() * 4) + 1);
+    document.getElementById("suits1").src = "suit-" + ranNum1 + ".png";
+    document.getElementById("suits2").src = "suit-" + ranNum2 + ".png";
+    document.getElementById("suits3").src = "suit-" + ranNum3 + ".png";
+    document.getElementById("suits4").src = "suit-" + ranNum4 + ".png";
+    if (ranNum1 == 4 || ranNum2 == 4 || ranNum3 == 4 || ranNum4 == 4) {
+        document.getElementById("result").innerHTML = "You Lose!";
+    }
+    else {
+        document.getElementById("result").innerHTML = "You Win!";
+    }
+}
