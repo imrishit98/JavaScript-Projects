adec6619d6c56536a4e68620b94bd772fac34f53
Rishit Patel <49016509+imrishit98@users.noreply.github.com>
2020-02-29 19:18:09 -0500

    Added rules,money and scores

--- Escape the spade/script.js
+++ Escape the spade/script.js
@@ -1,10 +1,41 @@
-/*
-    Author: Rishit Patel
-    Date: 20 Nov, 2019
-    Description: Escape The Spade Game
-*/
+var points = 0;
+function addMoney() {
+    points = prompt("Add money to play!");
+    document.getElementById("points").innerHTML = "$" + points;
+}
 function startGame() {
-    ranNum1 = Math.floor((Math.random() * 4) + 1);
+    var lose = 0;
+    var score = 0;
+    var count = 0;
+    document.getElementById("result").innerHTML = "You Win!";
+    for (count = 1; count < 5; count++) {
+        var num = Math.floor((Math.random() * 4) + 1);
+        document.getElementById("suits" + count).src = "suit-" + num + ".png";
+        if (num == 4) {
+            lose = 1;
+            document.getElementById("result").innerHTML = "You Lose!";
+        }
+        else if (num == 2 || num == 3) {
+            score += 3;
+        }
+        if (score == 12) {
+            score = score * 4;
+        }
+        if (score == 9) {
+            score = score * 3;
+        }
+    }
+    if (lose == 1) {
+        document.getElementById("score").innerHTML = 0;
+        points -= 8.5;
+    }
+    else {
+        points += score;
+        document.getElementById("score").innerHTML = score;
+    }
+    document.getElementById("points").innerHTML = "$" + points;
+
+    /*ranNum1 = Math.floor((Math.random() * 4) + 1);
     ranNum2 = Math.floor((Math.random() * 4) + 1);
     ranNum3 = Math.floor((Math.random() * 4) + 1);
     ranNum4 = Math.floor((Math.random() * 4) + 1);
@@ -17,5 +48,13 @@ function startGame() {
     }
     else {
         document.getElementById("result").innerHTML = "You Win!";
-    }
+    }*/
+}
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "351px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
 }
